f9855edbf10c8123112b18396362cdc9982c53e7
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-12-10 17:33:01 +0530

    Finishing loginPage!!

--- login.php
+++ login.php
@@ -24,12 +24,32 @@
         #bar2{
             background-color: white;
             width:800px; 
-            height: 400px; 
+            /* height: 200px;  */
             margin:auto;
             margin-top:50px;
             padding: 10px;
             text-align: center;
+            font-weight: bold;
         }
+        #text{
+            height: 40px;
+            width: 300px;
+            border-radius: 4px;
+            border:solid 1px #ccc;
+            padding: 4px;
+            font-size: 14px;
+        }
+
+        #button{
+            width: 300px;
+            height: 40px;
+            border-radius: 4px;
+            border: none;
+            background-color: rgb(59, 89, 152); 
+            color: white;
+            font-weight: bold;
+        }
+
     </style>
 </head>
 <body style="font-family: tahoma;  background-color: #e9ebee;">
@@ -40,9 +60,9 @@
 
     <div id="bar2">
     Log in to Mybook <br><br>
-    <input type="text" id="text"><br><br>
-    <input type="text" id="text"><br><br>
-    <input type="submit" id="button" value="Log In">
+    <input type="text" id="text" placeholder="Email"><br><br>
+    <input type="password" id="text" placeholder="Password"><br><br>
+    <input type="submit" id="button" value="Log In"><br><br><br>
     </div>
 </body>
 </html>
