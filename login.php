afd40cddc80af5b1587da79afc8eb8af759569ae
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-12-10 16:57:26 +0530

    Let's begin!

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mybook</title>
+    <style>
+        #bar{
+            height:100px;
+             background-color: rgb(59, 89, 152); 
+             color: #d9dfeb; 
+             font-size: 40px;
+        }
+    </style>
+</head>
+<body>
+    <div id="bar">
+        Mybook Signup
+    </div>
+</body>
+</html>
