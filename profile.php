ee7933ab58d0d6e6c84a9e364b07d42885e380c5
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-12-11 10:46:07 +0530

    Profile Page Top Bar

---
+++ profile.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile | Mybook</title>
+    <style>
+
+    #blue_bar{
+        height: 50px;
+        background-color: #405d9b;
+        color: #d9dfeb;
+    }
+    #search_box{
+        width: 400px;
+        height: 20px;
+        border-radius: 5px;
+        border: none;
+        padding: 4px;
+        font-size: 14px;
+        background-image: url(images/search.png);
+        background-repeat: no-repeat;
+        background-position: right;
+    }
+    </style>
+</head>
+<body style="font-family: tahoma;">
+    <div id="blue_bar">
+        <div style="width: 800px; margin:auto;font-size:30px">
+        Mybook &nbsp; &nbsp;
+    <input type="text" id="search_box" placeholder="Search for people"> 
+    <img src="images/selfie.jpg" alt="" style="width: 50px;float: right;">
+    </div>
+        </div>
+</body>
+</html>
