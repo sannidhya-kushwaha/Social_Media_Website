40dbc7d6bd44d74846dcc3b91f516a7e866d1dae
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-12-11 11:57:02 +0530

    Profile Page Posts

--- profile.php
+++ profile.php
@@ -75,6 +75,20 @@
             width: 50px;
         }
 
+        #post_bar{
+             margin-top: 20px;
+             background-color: white;
+             padding: 10px;
+
+        }
+        #post{
+            padding: 4px;
+            font-size: 13px;
+            display: flex;
+            margin-bottom: 20px;
+            
+        }
+
     </style>
 </head>
 <body style="font-family: tahoma; background-color:#d0d8e4">
@@ -148,6 +162,42 @@
                <input id="post_button" type="submit" value="Post">
                 <br>
             </div>
+
+
+         <!-- Posts -->
+            <div id="post_bar">
+
+              <!-- Post 1 -->
+                <div id="post">
+                    <div> 
+                        <img src="images/user1.jpg" alt="" style="width: 75px;margin-right: 4px;">
+                    </div>
+                    <div>
+                        <div style="font-weight: bold; color:#405d9b;" >First Guy</div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolor, illo possimus ab repellendus aspernatur qui tempore atque harum obcaecati quos accusamus et dolores dolorum nesciunt ea natus cum. Blanditiis corporis dolore nulla et.
+                        <br><br>
+                        <a href="">Like</a> . <a href="">Comment</a> . <span style="color: #999;">April 23 2020</span>
+                    </div>
+                </div>
+              <!-- Post 2 -->
+                <div id="post">
+                    <div> 
+                        <img src="images/user4.jpg" alt="" style="width: 75px;margin-right: 4px;">
+                    </div>
+                    <div>
+                        <div style="font-weight: bold; color:#405d9b;" >African Guy</div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolor, illo possimus ab repellendus aspernatur qui tempore atque harum obcaecati quos accusamus et dolores dolorum nesciunt ea natus cum. Blanditiis corporis dolore nulla et.
+                        <br><br>
+                        <a href="">Like</a> . <a href="">Comment</a> . <span style="color: #999;">April 23 2020</span>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
+
         </div>
 
 
