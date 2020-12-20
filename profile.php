7301f41c13014c53db913dcd499d3b189c153b6f
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-12-20 20:09:36 +0530

    Logout page

--- profile.php
+++ profile.php
@@ -135,6 +135,9 @@
     <input type="text" id="search_box" placeholder="Search for people"> 
     <!-- <img src="images/selfie.jpg" alt="" style="width: 50px;float: right;"> -->
     <img src="images/sannidhya.jpg" alt="" style="width: 50px;float: right;">
+    <a href="logout.php">
+    <span style="font-size: 11px; float: right; margin:10px;color: white;">Logout</span>
+    </a>
        
     </div>
         </div>
