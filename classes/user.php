36c3e2f9f06088ee32e652b7a0f41fbb36a90a43
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-12-21 12:40:00 +0530

    Post Design

--- classes/user.php
+++ classes/user.php
@@ -14,5 +14,18 @@ class User{
             return false;
         }
     }
+
+    public function get_user($id){
+
+        $query = "select * from users where userid = '$id' limit 1 ";
+        $DB = new Database();
+        $result = $DB->read($query);
+
+        if($result){
+            return $result[0];
+        }else{
+            return false;
+        }
+    }
     
 }
--- post.php
+++ post.php
@@ -1,11 +1,24 @@
+
 <div id="post">
                         <div> 
-                            <img src="images/user1.jpg" alt="" style="width: 75px;margin-right: 4px;">
+                            <?php 
+                               $image = "images/user_male.jpg";
+                               if($ROW_USER['gender'] == "Female"){
+                                   $image = "images/user_female.jpg";
+                               }
+
+                            ?>
+                            <img src="<?php echo $image ?>" alt="" style="width: 75px;margin-right: 4px;">
                         </div>
                         <div>
-                            <div style="font-weight: bold; color:#405d9b;" >First Guy</div>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolor, illo possimus ab repellendus aspernatur qui tempore atque harum obcaecati quos accusamus et dolores dolorum nesciunt ea natus cum. Blanditiis corporis dolore nulla et.
+                            <div style="font-weight: bold; color:#405d9b;" ><?php echo $ROW_USER['first_name'] . " " . $ROW_USER['last_name'] ?></div>
+                            <?php echo $ROW['post'] ?>
                             <br><br>
-                            <a href="">Like</a> . <a href="">Comment</a> . <span style="color: #999;">April 23 2020</span>
+                            <a href="">Like</a> . <a href="">Comment</a> . 
+                            <span style="color: #999;">
+                            
+                            <php echo $ROW['date'] ?>
+
+                        </span>
                         </div>
                     </div>
